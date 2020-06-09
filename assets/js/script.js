77b38e3a1d2e6010c1dc08b87ae66c3849d99ca2
psanketprdxn <sanketp@prdxn.com>
2020-06-09 18:01:43 +0530

    Work completed

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/*Authohr: Sanket Patil*/
+
+console.log('this is alarm mini project');
+
+let alarmTime = document.getElementById('alarmTime');
+let setAlarm = document.getElementById('setAlarm');
+let audio = new Audio('http://freshly-ground.com/data/audio/mpc/20150929%20-%20Amore%20(Dynamic%20Compressor%201.2.6).mp3');
+setAlarm.addEventListener('click', setAlarmTime); 
+
+function ringAlarm() {
+  console.log('audio is ringing...');
+  audio.play();
+}
+
+function setAlarmTime() {
+  let alarmValue = new Date(alarmTime.value);
+  let currentTime = new Date();
+  alarmTime.value = "";
+  let timeValue = alarmValue - currentTime;
+  console.log(timeValue);
+  
+  if(timeValue >= 0) {
+    setTimeout(function(){
+      ringAlarm();
+    }, timeValue);
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
